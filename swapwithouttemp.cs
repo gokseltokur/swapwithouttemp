5daac10c32e7c55f9d610e723f6085600c15da33
Göksel Tokur <gokseltokur@gmail.com>
2019-02-25 01:49:03 +0300

    add

---
+++ swapwithouttemp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int x, y;
+        Console.WriteLine("Enter the value of x");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of y");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        Console.Write("New values are : \n x = " + x + "\n y = " + y);
+    }
+}
